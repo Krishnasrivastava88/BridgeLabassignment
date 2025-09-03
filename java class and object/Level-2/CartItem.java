7c65a93d786a16f2c1513a8e33de33b11b6aefbc
Krishnasrivastava88 <krissh8858@gmail.com>
2025-09-04 00:00:15 +0530

    Added CartItem.java in Level-2

---
+++ java class and object/Level-2/CartItem.java
@@ -0,0 +1,24 @@
+class CartItem {
+    String itemName;
+    double price;
+    int quantity;
+
+    double totalCost() {
+        return price * quantity;
+    }
+
+    void display() {
+        System.out.println("Item: " + itemName);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total: " + totalCost());
+    }
+
+    public static void main(String[] args) {
+        CartItem item = new CartItem();
+        item.itemName = "Notebook";
+        item.price = 50;
+        item.quantity = 3;
+        item.display();
+    }
+}
