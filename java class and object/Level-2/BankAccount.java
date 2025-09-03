cc867de970173a550a4f4a88f2c8d71efa04ac44
Krishnasrivastava88 <krissh8858@gmail.com>
2025-09-03 23:54:17 +0530

    Added BankAccount.java in Level-2

---
+++ java class and object/Level-2/BankAccount.java
@@ -0,0 +1,28 @@
+class BankAccount {
+    String accountHolder;
+    int accountNumber;
+    double balance;
+
+    void deposit(double amount) {
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if(amount <= balance) balance -= amount;
+        else System.out.println("Insufficient balance!");
+    }
+
+    void displayBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        acc.accountHolder = "Krishna";
+        acc.accountNumber = 12345;
+        acc.balance = 1000;
+        acc.deposit(500);
+        acc.withdraw(200);
+        acc.displayBalance();
+    }
+}
