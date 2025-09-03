7755c3753b428df643975baceeedfdb28032eaff
Krishnasrivastava88 <krissh8858@gmail.com>
2025-09-03 15:40:29 +0530

    Added Employee.java in Level-1

---
+++ java class and object/Level-1/Employee.java
@@ -0,0 +1,19 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.name = "Krishna";
+        emp.id = 101;
+        emp.salary = 50000;
+        emp.displayDetails();
+    }
+}
