1feda49d25a154a2515a7b316f7ec8d26570a17b
Krishnasrivastava88 <krissh8858@gmail.com>
2025-09-04 16:14:46 +0530

    [Krishna] Add: New files in Instances folder

---
+++ JavaConstructors/Instances/Vehicle.java
@@ -0,0 +1,20 @@
+class Vehicle {
+    String ownerName;
+    String vehicleType;
+    static double registrationFee = 500.0;
+
+    Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    void displayVehicleDetails() {
+        System.out.println("Owner: " + ownerName + ", Vehicle: " + vehicleType +
+                           ", Registration Fee: " + registrationFee);
+    }
+
+    static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+}
+
